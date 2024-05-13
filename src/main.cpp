d5a1756ce641b050091d419c9e030e10bde1437b
vapid4me <augustxun@outlook.com>
2024-05-12 23:27:28 -0400

    可以读数据

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 // #include "stm32f429i_discovery_lcd.h"  // 包含LCD显示库，用于显示屏设置
 #include <drivers/LCD_DISCO_F429ZI.h>
 
-#define WINDOW_SIZE 256  // Window size
+
 
 // Define Regs & Configurations --> Gyroscope's settings
 #define CTRL_REG1 0x20
@@ -41,14 +41,14 @@
 // 定义震颤检测的频率范围3-6和阈值, 该阈值用于消抖 (阈值先设为1.0f,可以根据需求和实际数据更改)
 #define TREMOR_FREQ_LOW 3
 #define TREMOR_FREQ_HIGH 6
-#define THRESHOLD 1.0f
+#define THRESHOLD_LOW 0.2f
+#define THRESHOLD_HIGH 10.0f;
 
 // EventFlags object declaration
 EventFlags flags;
 
 
-
-
+#define WINDOW_SIZE 256  // Window size
 float32_t fft_input_x[WINDOW_SIZE * 2];  // 定义x轴FFT输入数组
 float32_t fft_input_y[WINDOW_SIZE * 2];  // 定义y轴FFT输入数组
 float32_t fft_input_z[WINDOW_SIZE * 2];  // 定义z轴FFT输入数组
@@ -56,6 +56,8 @@ float32_t fft_output_x[WINDOW_SIZE];  // 定义x轴FFT输出数组
 float32_t fft_output_y[WINDOW_SIZE];  // 定义y轴FFT输出数组
 float32_t fft_output_z[WINDOW_SIZE];  // 定义z轴FFT输出数组
 arm_rfft_fast_instance_f32 s;  // FFT实例
+LCD_DISCO_F429ZI lcd;  // LCD实例
+
 
 // spi callback function
 void spi_cb(int event) {
@@ -72,7 +74,6 @@ void init_fft() {
 }
 
 void init_lcd() {
-   LCD_DISCO_F429ZI lcd;  // 初始化LCD
    lcd.SetBackColor(LCD_COLOR_WHITE);  // 设置白色背景色
    lcd.SetTextColor(LCD_COLOR_BLACK);  // 设置黑色文字色
    lcd.DisplayStringAt(0, LINE(3), (uint8_t *)"Detection Active", CENTER_MODE);  // 显示文字(Active)
@@ -102,8 +103,7 @@ void analyze_tremor(float32_t* fft_output_x, float32_t* fft_output_y, float32_t*
         float strength_y = sqrtf(fft_output_y[i] * fft_output_y[i]); // 计算y轴频率的强度
         float strength_z = sqrtf(fft_output_z[i] * fft_output_z[i]); // 计算z轴频率的强度
         float combined_strength = (strength_x + strength_y + strength_z) / 3;  // 使用平均值综合三个轴的强度
-        printf("tremor:%4.5f\n",combined_strength);
-        if (combined_strength > THRESHOLD) { // 检查强度是否超过阈值 (去抖)
+        if (combined_strength > THRESHOLD_LOW) { // 检查强度是否超过阈值 (去抖)
             tremor_detected = true; // 超过则检测到震颤，改震颤检测状态为是
             tremor_strength = max(tremor_strength, combined_strength); // 更新最大震颤强度
         }
@@ -113,16 +113,18 @@ void analyze_tremor(float32_t* fft_output_x, float32_t* fft_output_y, float32_t*
 // 更新显示内容的函数
 void update_lcd_display(bool tremor_detected, float tremor_strength) {
     if (tremor_detected) {
-        BSP_LCD_Clear(LCD_COLOR_RED);  // 如果检测到震颤，将背景设为红色
-        BSP_LCD_SetTextColor(LCD_COLOR_WHITE);  // 文字颜色设为白色
-        BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Tremor Detected!", CENTER_MODE);  // 显示检测到震颤
+        lcd.Clear(LCD_COLOR_WHITE);
+        lcd.SetBackColor(LCD_COLOR_RED);  // 如果检测到震颤，将背景设为红色
+        lcd.SetTextColor(LCD_COLOR_WHITE);  // 文字颜色设为白色
+        lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"Tremor Detected!", CENTER_MODE);  // 显示检测到震颤
         char buf[32];
-        sprintf(buf, "Strength: %.2f Hz", tremor_strength);  // 震颤强度保留至小数点后两位输出
-        BSP_LCD_DisplayStringAt(0, LINE(6), (uint8_t *)buf, CENTER_MODE);  // 显示震颤强度文字
+        snprintf(buf, 32, "Strength: %.2f Hz", tremor_strength);  // 震颤强度保留至小数点后两位输出
+        lcd.DisplayStringAt(0, LINE(6), (uint8_t *)buf, CENTER_MODE);  // 显示震颤强度文字
     } else {
-        BSP_LCD_Clear(LCD_COLOR_GREEN);  // 如果未检测到震颤，则将背景设为绿色
-        BSP_LCD_SetTextColor(LCD_COLOR_BLACK);  // 文字颜色设为黑色
-        BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"No Tremor", CENTER_MODE);  // 显示未检测到震颤
+        lcd.Clear(LCD_COLOR_WHITE);
+        lcd.SetBackColor(LCD_COLOR_GREEN);  // 如果未检测到震颤，则将背景设为绿色
+        lcd.SetTextColor(LCD_COLOR_BLACK);  // 文字颜色设为黑色
+        lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"No Tremor", CENTER_MODE);  // 显示未检测到震颤
     }
 }
 
@@ -130,6 +132,7 @@ void update_lcd_display(bool tremor_detected, float tremor_strength) {
 
 int main() {
     init_lcd();
+    init_fft();
     //spi initialization
     SPI spi(PF_9, PF_8, PF_7, PC_1, use_gpio_ssel);
     uint8_t write_buf[32], read_buf[32];
@@ -170,6 +173,7 @@ int main() {
 
     float filtered_gx = 0.0f, filtered_gy = 0.0f, filtered_gz = 0.0f;
     int cycle=0;
+    int cnt=1;
     while (1) {
         int16_t raw_gx, raw_gy, raw_gz;
         float gx, gy, gz;
@@ -195,28 +199,28 @@ int main() {
 
         // printf("RAW -> \t\tgx: %d \t gy: %d \t gz: %d\t\n", raw_gx, raw_gy, raw_gz);
 
-        printf(">x_axis_low:%4.5f\n", filtered_gx);
-        printf(">y_axis_low:%4.5f\n", filtered_gy);
-        printf(">z_axis_low:%4.5f\n", filtered_gz);
-        printf("%d\n", cycle);
+        // printf(">x_axis_low:%4.5f\n", filtered_gx);
+        // printf(">y_axis_low:%4.5f\n", filtered_gy);
+        // printf(">z_axis_low:%4.5f\n", filtered_gz);
 
-        fft_input_x[2 * cycle] = (float)filtered_gx * SCALING_FACTOR;  // 存储处理后的x轴数据
-        fft_input_y[2 * cycle] = (float)filtered_gy * SCALING_FACTOR;  // 存储处理后的y轴数据
-        fft_input_z[2 * cycle] = (float)filtered_gz * SCALING_FACTOR;  // 存储处理后的z轴数据
+        fft_input_x[2 * cycle] = (float)filtered_gx;  // 存储处理后的x轴数据
+        fft_input_y[2 * cycle] = (float)filtered_gy;  // 存储处理后的y轴数据
+        fft_input_z[2 * cycle] = (float)filtered_gz;  // 存储处理后的z轴数据
         
         if (cycle==255) {
-            printf("finish reading");
-            // arm_rfft_fast_f32(&s, fft_input_x, fft_output_x, 0);  // 对x轴数据进行FFT
-            // arm_rfft_fast_f32(&s, fft_input_y, fft_output_y, 0);  // 对y轴数据进行FFT
-            // arm_rfft_fast_f32(&s, fft_input_z, fft_output_z, 0);  // 对z轴数据进行FFT
+            printf("%d-Finish Reading\n", cnt);
+            arm_rfft_fast_f32(&s, fft_input_x, fft_output_x, 0);  // 对x轴数据进行FFT
+            arm_rfft_fast_f32(&s, fft_input_y, fft_output_y, 0);  // 对y轴数据进行FFT
+            arm_rfft_fast_f32(&s, fft_input_z, fft_output_z, 0);  // 对z轴数据进行FFT
 
-            // float tremor_strength;
-            // bool tremor_detected;
-            // //analyze_tremor(fft_output_x, tremor_strength, tremor_detected); // 这是只分析x轴的分析方法
-            // analyze_tremor(fft_output_x, fft_output_y, fft_output_z, tremor_strength, tremor_detected);  // 调用函数综合分析三个轴的震颤
-            // update_lcd_display(tremor_detected, tremor_strength);  // 调用函数更新显示
+            float tremor_strength=0.0; bool tremor_detected;
+            analyze_tremor(fft_output_x, fft_output_y, fft_output_z, tremor_strength, tremor_detected);  // 调用函数综合分析三个轴的震颤
+            if (tremor_detected) printf("%4.5f\n", tremor_strength);
+            printf("%d-Finish Analyzing\n", cnt);
+            cnt++;  
+            update_lcd_display(tremor_detected, tremor_strength);  // 调用函数更新显示
 
-            // ThisThread::sleep_for(1000);  // 每秒更新一次
+            ThisThread::sleep_for(1000);  // 每秒更新一次
             cycle = 0;
             continue;
         }
